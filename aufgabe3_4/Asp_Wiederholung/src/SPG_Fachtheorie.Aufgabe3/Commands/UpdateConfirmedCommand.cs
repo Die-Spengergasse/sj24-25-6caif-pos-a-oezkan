32f6797d499efe8e356186da6a50b9543735f63a
Arda Özkan <Oez21976@spengergasse.at>
2025-03-26 15:23:52 +0100

    Aufgabe 5

---
+++ aufgabe3_4/Asp_Wiederholung/src/SPG_Fachtheorie.Aufgabe3/Commands/UpdateConfirmedCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SPG_Fachtheorie.Aufgabe3.Commands
+{
+    public record UpdateConfirmedCommand : IValidatableObject
+    {
+        public DateTime? Confirmed { get; init; }
+        public UpdateConfirmedCommand() { }
+
+        public UpdateConfirmedCommand(DateTime? confirmed)
+        {
+            Confirmed = confirmed;
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Confirmed.HasValue)
+            {
+                var now = DateTime.UtcNow;
+                if (Confirmed.Value > now.AddMinutes(1))
+                {
+                    yield return new ValidationResult(
+                        "Confirmed cannot be more than 1 minute in the future.",
+                        new[] { nameof(Confirmed) }
+                    );
+                }
+            }
+        }
+    }
+}
---
+++ aufgabe3_4/Asp_Wiederholung/src/SPG_Fachtheorie.Aufgabe3/Commands/UpdatePaymentItemCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SPG_Fachtheorie.Aufgabe3.Commands
+{
+    public record UpdatePaymentItemCommand(
+        [Range(1, int.MaxValue, ErrorMessage = "ID must be > 0")]
+        int Id,
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "ArticleName is required")]
+        string ArticleName,
+
+        // amount > 0
+        [Range(1, int.MaxValue, ErrorMessage = "Amount must be > 0")]
+        int Amount,
+
+        // price > 0
+        [Range(typeof(decimal), "0.01", "9999999999", ErrorMessage = "Price must be > 0")]
+        decimal Price,
+
+        // paymentId > 0
+        [Range(1, int.MaxValue, ErrorMessage = "PaymentId must be > 0")]
+        int PaymentId,
+
+        DateTime? LastUpdated
+    );
+}
