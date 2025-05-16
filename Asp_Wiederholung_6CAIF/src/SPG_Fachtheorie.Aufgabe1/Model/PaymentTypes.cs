3dc37cca0734c149024d0caa6ff299f2b89050c0
Arda Özkan <Oez21976@spengergasse.at>
2025-05-16 16:30:42 +0200

    Aufgabe 8

---
+++ Asp_Wiederholung_6CAIF/src/SPG_Fachtheorie.Aufgabe1/Model/PaymentTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace SPG_Fachtheorie.Aufgabe1.Model
+{
+    public enum PaymentType
+    {  
+        Cash,         // 0
+        Maestro,      // 1
+        CreditCard    // 2
+    }
+}
---
+++ Asp_Wiederholung_6CAIF/src/SPG_Fachtheorie.Aufgabe3/Controllers/PaymentsController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SPG_Fachtheorie.Aufgabe1.Commands;
+using SPG_Fachtheorie.Aufgabe1.Infrastructure;
+using SPG_Fachtheorie.Aufgabe1.Model;
+using SPG_Fachtheorie.Aufgabe1.Services;
+using SPG_Fachtheorie.Aufgabe3.Commands;
+using SPG_Fachtheorie.Aufgabe3.Dtos;
+
+namespace SPG_Fachtheorie.Aufgabe3.Controllers
+{
+    [Route("api/[controller]")]  // --> api/payments
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly PaymentService _service;
+
+        public PaymentsController(PaymentService service)
+        {
+            _service = service;
+        }
+
+        /// <summary>
+        /// GET /api/payments
+        /// GET /api/payments?cashDesk=1
+        /// GET /api/payments?dateFrom=2024-05-13
+        /// GET /api/payments?dateFrom=2024-05-13&cashDesk=1
+        /// </summary>
+        [HttpGet]
+        public ActionResult<List<PaymentDto>> GetAllPayments(
+            [FromQuery] int? cashDesk,
+            [FromQuery] DateTime? dateFrom)
+        {
+            var payments = _service.Payments
+                .Where(p => (!cashDesk.HasValue || p.CashDesk.Number == cashDesk.Value)
+                         && (!dateFrom.HasValue || p.PaymentDateTime >= dateFrom.Value))
+                .Select(p => new PaymentDto(
+                    p.Id, p.Employee.FirstName, p.Employee.LastName,
+                    p.PaymentDateTime,
+                    p.CashDesk.Number, p.PaymentType.ToString(),
+                    p.PaymentItems.Sum(pi => pi.Price)))
+                .ToList();
+            return Ok(payments);
+        }
+
+        /// <summary>
+        /// GET /api/payments/{id}
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ActionResult<PaymentDetailDto> GetPaymentById(int id)
+        {
+            var payment = _service.Payments
+                .Where(p => p.Id == id)
+                .Select(p => new PaymentDetailDto(
+                    p.Id, p.Employee.FirstName, p.Employee.LastName,
+                    p.CashDesk.Number, p.PaymentType.ToString(),
+                    p.PaymentItems
+                        .Select(pi => new PaymentItemDto(
+                            pi.ArticleName, pi.Amount, pi.Price))
+                        .ToList()))
+                .FirstOrDefault();
+            if (payment is null) return NotFound();
+            return Ok(payment);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult AddPayment([FromBody] NewPaymentCommand cmd)
+        {
+            try
+            {
+                var payment = _service.CreatePayment(cmd);
+                return CreatedAtAction(nameof(AddPayment), new { payment.Id });
+            }
+            catch (PaymentServiceException e)
+            {
+                return Problem(e.Message, statusCode: 400);
+            }
+        }
+
+        /// <summary>
+        /// DELETE /api/payments/{id}?deleteItems=true|false
+        /// </summary>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult DeletePayment(int id, [FromQuery] bool deleteItems)
+        {
+            try
+            {
+                _service.DeletePayment(id, deleteItems);
+                return NoContent();
+            }
+            catch (PaymentServiceException e) when (e.IsNotFoundError)
+            {
+                return Problem(e.Message, statusCode: 404);
+            }
+            catch (PaymentServiceException e)
+            {
+                return Problem(e.Message, statusCode: 400);
+            }
+        }
+
+        //[HttpPut("/api/paymentItems/{id}")]
+        //public IActionResult UpdatePayment(int id, [FromBody] UpdatePaymentItemCommand cmd)
+        //{
+        //    if (cmd.Id != id)
+        //        return Problem("Invalid payment item id", statusCode: 400);
+
+        //    var paymentItem = _db.PaymentItems.FirstOrDefault(p => p.Id == cmd.Id);
+        //    if (paymentItem is null) return Problem("Payment item Item not found", statusCode: 404);
+
+        //    var payment = _db.Payments.FirstOrDefault(p => p.Id == cmd.PaymentId);
+        //    if (payment is null) return Problem("Payment Item not found", statusCode: 404);
+
+        //    if (paymentItem.LastUpdated != cmd.LastUpdated)
+        //        return Problem("Payment item has changed", statusCode: 400);
+
+        //    paymentItem.ArticleName = cmd.ArticleName;
+        //    paymentItem.Price = cmd.Price;
+        //    paymentItem.Payment = payment;
+        //    paymentItem.LastUpdated = DateTime.UtcNow;
+        //    try
+        //    {
+        //        _db.SaveChanges();
+        //    }
+        //    catch (DbUpdateException e)
+        //    {
+        //        return Problem(e.InnerException?.Message ?? e.Message, statusCode: 400);
+        //    }
+        //    return NoContent();
+        //}
+
+        [HttpPatch("{id}")]
+        public IActionResult UpdateConfirmed(int id)
+        {
+            try
+            {
+                _service.ConfirmPayment(id);
+                return NoContent();
+            }
+            catch (PaymentServiceException e) when (e.IsNotFoundError)
+            {
+                return Problem(e.Message, statusCode: 404);
+            }
+            catch (PaymentServiceException e)
+            {
+                return Problem(e.Message, statusCode: 400);
+            }
+        }
+    }
+}
